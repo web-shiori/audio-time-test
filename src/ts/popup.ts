9dd1db8e8255fac536e82588da2ad150a2d9daa7
masatora <atarashi.masatora@gmail.com>
2022-01-04 20:18:54 +0900

    音声の再生位置取得

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,1 +1,21 @@
-alert("Hello, World")
+function getAudioPlayBackPosition() {
+    chrome.tabs.query(
+        { active: true, lastFocusedWindow: true },
+        function (tabs) {
+            chrome.tabs.executeScript(
+                <number>tabs[0].id,
+                {
+                    code: `document.getElementsByTagName('audio')[0].currentTime;`,
+                },
+                (result) => {
+                    const audioPlayBackPosition: number = Number(
+                        result[0]
+                    );
+                    alert(audioPlayBackPosition)
+                }
+            );
+        }
+    );
+}
+
+getAudioPlayBackPosition()
